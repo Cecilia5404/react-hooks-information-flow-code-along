440a531696ed78cad2d7fd316f4e6eeb5968452b
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-07-17 18:08:35 +0300

    commit tree 3ed5d2d6befcbc18aabc3c026912b8eb8a3e3251

--- src/Child.js
+++ src/Child.js
@@ -3,6 +3,20 @@ import { getRandomColor } from "./randomColorGenerator.js";
 
 function Child({ onChangeColor }) {
   return <div className="child" style={{ backgroundColor: "#FFF" }} />;
-}
+function Child({ onChangeColor, color }) {
+  // console.log(onChangeColor);
+  function handleClick() {
+    const newColor = getRandomColor();
+    onChangeColor(newColor);
+  }
 
+  return (
+    <div
+      onClick={handleClick}
+      className="child"
+      style={{ backgroundColor: color }}
+    />
+  );
+}
+}
 export default Child;
